97c2d8efd34536a3d2f690b20cee923e7e18c31f
Maciej Ciechanowski <ciechan92@gmail.com>
2022-05-09 13:20:45 +0200

    Methods for simply numbers

--- src/pl/interview/tasks/Main.java
+++ src/pl/interview/tasks/Main.java
@@ -14,23 +14,53 @@ public class Main {
 
     public static void main(String[] args) {
         int rangeOfSearch = 10000;
+        int numberOfLychrelNumbers = 0;
 
-        System.out.println(checkingPalindromicNumber(1221)); //test
+        for(int i = 0; i<rangeOfSearch; i++) {
+            if (checkingPalindromicNumber(i))
+                numberOfLychrelNumbers += 1;
+        }
+        System.out.println("Number of Lychrel Numbers below 10000: " + numberOfLychrelNumbers);
 
     }
 
     public static boolean checkingPalindromicNumber (int number){
-        String checkingNumber = String.valueOf(number);
-        String reverseCheckingNumber = "";
+        int countIteration = 10;
+        String firstNumber = String.valueOf(number);
+        String reverseNumber = "";
+        String checkingNumber = "";
 
-        for(int i = checkingNumber.length()-1; i>=0; i--) {
-            reverseCheckingNumber = reverseCheckingNumber + checkingNumber.charAt(i);
+        for (int i=1; i<=countIteration; i++){
+            reverseNumber = reverseNumber(firstNumber);
+            checkingNumber = String.valueOf( Integer.valueOf(firstNumber) + Integer.valueOf(reverseNumber) );
+
+//            System.out.println(firstNumber);
+//            System.out.println(reverseNumber);
+//            System.out.println(checkingNumber);
+//            System.out.println();
+
+            if (reverseNumber(checkingNumber).equals(checkingNumber)) {
+//                System.out.println("Number: " + number + "   Count interations: " + i + "   Palindromic number: " + checkingNumber);
+                return false;
+            } else if (i == countIteration){
+                System.out.println("Lychrel number: " + number + "   (Performed iterations: " + i + ")");
+            }
+
+            firstNumber = checkingNumber;
+
         }
 
-        if (reverseCheckingNumber.equals(checkingNumber))
-            return true;
-        else
-            return false;
+        return true;
+    }
+
+    public static String reverseNumber(String number){
+        String reverseNumber = "";
+
+        for(int i = number.length()-1; i>=0; i--) {
+            reverseNumber = reverseNumber + number.charAt(i);
+        }
+
+        return  reverseNumber;
     }
 
 }
